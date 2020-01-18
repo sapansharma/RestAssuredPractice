18c9c03f721f869e2ee1cdae5f02757bbff67970
Sapan Sharma <sapan.sharma@onedirect.in>
2020-01-18 17:12:09 +0530

    RestBasics
    RestBasics Changes

---
+++ ResAssuredPractice/src/RABasics01.java
@@ -0,0 +1,20 @@
+//	import static io.restassured.RestAssured.given;
+//	import static org.hamcrest.Matchers.equalTo;
+//
+//	import org.testng.annotations.Test;
+//
+
+//	import io.restassured.http.ContentType;
+
+
+
+public class RABasics01 {
+	
+	public void getCall(){
+		
+		RestAssured.baseURI="";
+		
+	}
+
+}
+
